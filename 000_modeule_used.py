58f26fa0143560f0ea19e046a99dc4957968937a
AmirRahmat786 <amirrahmatn@gmail.com>
2024-07-29 18:00:47 +0500

    adding my new file

---
+++ 000_modeule_used.py
@@ -0,0 +1,25 @@
+"""               which modeule for used 
+
+1 import qrcode                      used for make qr
+2 from tkinter import*               used for graphics (class ttk for combo box)
+3 import os                          used for operating system
+4 import random                      used for random task
+5 from time import*                  used for time limit Check
+6 import speedtest                   used for internet speed check
+7 import pywhatkit                   used for text_to_handwriting     
+8 from pytube import YouTube         used for youtube   
+9 from pill import image (pip install pillow)            used for image  
+10 import phonenumbers   (pip install phonenumbers)     
+11 import player (pip install player)      
+12 from instabot import bot  (pip install instabot) used for.....
+..... follow,unfollow, upload pic,get info of any id
+13 from plyer import notification (pip install plyer) used for notify
+14 import datetime (pip install datetime)used for date and tme
+15 import time (pip install time)    used for time
+16 from skpy import skype (pip install skpy)used for skype automation
+17 import pyttsx3   used for text convert in speach   
+18 import speech_recognition 
+19 import webbrowser
+20 import pyjokes
+21 import datetime
+22 import smtplib     (used for send email) """
